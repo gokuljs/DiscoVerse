92b79edc6365382c923ef7685c054a2f9412c8e6
gokuljs <gokuljs029@gmail.com>
2023-11-22 00:15:01 +0530

    auth

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div className="text-3xl font-bold text-indigo-500">Discord</div>;
+  return (
+    <div className="text-3xl font-bold text-indigo-500 flex justify-between items-center">
+      Discord <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
