4b5aca8821a1e9c7aca0c025384926de05d7e3e9
gokuljs <gokuljs029@gmail.com>
2023-12-21 22:06:20 +0530

    role configuerations

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -3,92 +3,60 @@
 import {
     Dialog,
     DialogContent,
+    DialogDescription,
     DialogHeader,
     DialogTitle
 } from '@/components/ui/dialog';
 import { useModal } from '../hooks/us-modal-store';
-import { Label } from '../ui/label';
-import { Input } from '../ui/input';
-import { Button } from '../ui/button';
-import { Check, Copy, RefreshCw } from 'lucide-react';
-import { useOrigin } from '../hooks/use-origin';
+import { ServerWithProfileWithMembers } from '@/types';
+import { ScrollArea } from '../ui/scroll-area';
+import UserAvatar from '../user-avatar';
+import { ShieldAlert, ShieldCheck } from 'lucide-react';
 import { useState } from 'react';
-import axios from 'axios';
+
+const roleIconMap = {
+    GUEST: null,
+    MODERATOR: <ShieldCheck className='h-4 w-4 ml-2 text-indigo-500' />,
+    ADMIN: <ShieldAlert className='h-4 w-4 ml-2 text-rose-500' />
+};
 
 export const MembersModal = () => {
     const { isOpen, type, onClose, data, onOpen } = useModal();
-    const origin = useOrigin();
-    const [copied, setCopied] = useState<boolean>(false);
-    const [isLoading, SetIsLoading] = useState<boolean>(false);
+    const [loadingId, setIsLoadingId] = useState('');
     const isModalOpen = isOpen && type === 'members';
-    const { server } = data;
-    const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
-
-    const onCopy = (): void => {
-        navigator.clipboard.writeText(inviteUrl);
-        setCopied(true);
-        setTimeout(() => {
-            setCopied(false);
-        }, 1000);
-    };
-
-    const regenerate = async () => {
-        try {
-            SetIsLoading(true);
-            const response = await axios.patch(
-                `/api/servers/${server?.id}/invite-code`
-            );
-            onOpen('invite', {
-                server: response.data
-            });
-        } catch (error) {
-            console.log(error);
-        } finally {
-            SetIsLoading(false);
-        }
-    };
+    const { server } = data as { server: ServerWithProfileWithMembers };
 
     return (
         <Dialog open={isModalOpen} onOpenChange={onClose}>
-            <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+            <DialogContent className='bg-white text-black overflow-hidden'>
                 <DialogHeader className='p-8 px-6'>
                     <DialogTitle className='text-2xl text-center font-bold'>
-                        Invite Friends
+                        Manage Members
                     </DialogTitle>
+                    <DialogDescription className='text-center text-zinc-500'>
+                        {server?.members?.length}
+                        Members
+                    </DialogDescription>
                 </DialogHeader>
-                <div className='p-6'>
-                    <Label className='uppercase font-bold text-xs text-zinc-500 dark:text-secondary/70'>
-                        server invite link
-                    </Label>
-                    <div className='flex items-center mt-2 gap-x-2'>
-                        <Input
-                            disabled={isLoading}
-                            className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
-                            value={inviteUrl}
-                        />
-                        <Button
-                            size='icon'
-                            onClick={onCopy}
-                            disabled={isLoading}
+                <ScrollArea className='mt-8 max-h-[420px] pr-6'>
+                    {server?.members?.map((member) => (
+                        <div
+                            key={member.id}
+                            className='flex items-center gap-x-2 mb-6'
                         >
-                            {copied ? (
-                                <Check className='w-4 h-4' />
-                            ) : (
-                                <Copy className='w-4 h-4' />
-                            )}
-                        </Button>
-                    </div>
-                    <Button
-                        disabled={isLoading}
-                        variant='link'
-                        size='sm'
-                        className='text-xs text-zinc-500 mt-4'
-                        onClick={regenerate}
-                    >
-                        Generate a new link
-                        <RefreshCw className='w-4 h-4 ml-2' />
-                    </Button>
-                </div>
+                            <UserAvatar src={member.profile.imageUrl} />
+                            <div className='flex flex-col gap-y-1'>
+                                <div className='text-xs font-semibold flex items-center gap-x-1'>
+                                    {member.profile.name}
+                                    {roleIconMap[member.role]}
+                                </div>
+                                <p className='text-xs text-zinc-500'>
+                                    {member.profile.email}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </ScrollArea>
             </DialogContent>
         </Dialog>
     );
